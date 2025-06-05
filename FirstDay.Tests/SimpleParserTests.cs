6472e4417ad367943cea87a47c4fbf8c3a93cf25
Eslam Shaban <eslamshaban69815@gmail.com>
2025-06-05 08:26:50 +0300

    add setup method

--- FirstDay.Tests/SimpleParserTests.cs
+++ FirstDay.Tests/SimpleParserTests.cs
@@ -4,10 +4,18 @@ namespace FirstDay.Tests;
 [TestFixture]
 public class SimpleParserTests
 {
+    private SimpleParser parser;
+    
+    [SetUp]
+    public void SetUp()
+    {
+         parser = new SimpleParser();
+    }
+    
     [Test]
     public void ParseAndSum_EmptyString_ReturnsZero()
     {
-        var parser = new SimpleParser();
+
         var result = parser.ParseAndSum("");
         Assert.That(result, Is.EqualTo(0));
     }
@@ -15,7 +23,6 @@ public class SimpleParserTests
     [Test]
     public void ParseAndSum_SingleNumber_ReturnsNumber()
     {
-        var parser = new SimpleParser();
         var result = parser.ParseAndSum("12");
         Assert.That(result, Is.EqualTo(12));
     }
@@ -23,14 +30,12 @@ public class SimpleParserTests
     [Test]
     public void ParseAndSum_MultipleNumbers_ThrowsInvalidOperation()
     {
-        var parser = new SimpleParser();
         Assert.That(() => parser.ParseAndSum("1,2") ,Throws.TypeOf<InvalidOperationException>());
     }
 
     [Test]
     public void NoRepeat_RemovesAllTargetChars()
     {
-        var parser = new SimpleParser();
         var result = parser.NoRepeat("banana", 'a');
         Assert.That(result, Is.EqualTo("bnn"));
     }
@@ -38,7 +43,6 @@ public class SimpleParserTests
     [Test]
     public void NoRepeat_EmptyInput_ReturnsEmpty()
     {
-        var parser = new SimpleParser();
         var result = parser.NoRepeat("", 'x');
         Assert.That(result, Is.EqualTo(string.Empty));
     }
@@ -46,7 +50,6 @@ public class SimpleParserTests
     [Test]
     public void NoRepeat_CharNotFound_ReturnsOriginal()
     {
-        var parser = new SimpleParser();
         var result = parser.NoRepeat("hello", 'z');
         Assert.That(result, Is.EqualTo("hello").IgnoreCase);
     }
@@ -58,7 +61,6 @@ public class SimpleParserTests
     [TestCase(10000000, false)]
     public void IsPrime_ValidInputs_ReturnsExpected(int input, bool expected)
     {
-        var parser = new SimpleParser();
         bool result = parser.IsPrime(input);
         Assert.That(result, Is.EqualTo(expected));
     }
@@ -68,7 +70,6 @@ public class SimpleParserTests
     [TestCase(100000001)]
     public void IsPrime_InvalidInputs_ThrowsException(int input)
     {
-        var parser = new SimpleParser();
         Assert.Throws<ArgumentOutOfRangeException>(() => parser.IsPrime(input));
     }
 }
